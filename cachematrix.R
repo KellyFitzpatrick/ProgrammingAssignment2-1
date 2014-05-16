45acbbe351cce21f9d358be45c2bce1b1d4261ff
Kelly Fitzpatrick <kfitzpatrick@ccm.edu>
2014-05-15 21:38:12 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
-
+## R code for Inverse of a Matrix
+## The version was written by Kelly Fitzpatrick 
+## The overall goal of this script is to take the inverse of an invertible square matrix
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
